79b1c6b48b76e478a49747e15c9942e8494eb45a
Prince Ofori <princeofori723@gmail.com>
2022-09-08 16:12:54 -0700

    solution

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ * * main - Write '"Programming is like building a multilingual puzzle'
+ *
+ * *Return: Always 0 (success)
+ *
+ * */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}

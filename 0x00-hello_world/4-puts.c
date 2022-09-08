88d8111ca11f1948f9dac3923366e92facc60f8b
Prince Ofori <princeofori723@gmail.com>
2022-09-08 16:55:09 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints the string in the put function
+ * Description: using the main function
+ * this c program prints Programming is like building a multilingual puzzle
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

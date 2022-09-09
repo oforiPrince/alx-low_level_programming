4a72fbc2021de44932f4ce1fe49d15652bf565e0
Prince Ofori <princeofori723@gmail.com>
2022-09-08 18:01:17 -0700

    correction on task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * main - print string using write
- * Description: a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19
+ * Description: a C program that prints using write
  * Return: 1
  */
 int main(void)

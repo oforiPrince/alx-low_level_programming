304bf981549307e450272096cf1ea0346b986805
Prince Ofori <princeofori723@gmail.com>
2022-09-09 19:12:12 -0700

    solution task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,12 +11,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i <=57; i++);
 	{
-		printf("%i", i);
+		putchar(i);
 	}
-
-	printf("\n");
+	putchar('\n');
 
 	return (0);
 }

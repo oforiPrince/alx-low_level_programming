3827535ef08d416daf3f93e06234353df866e3aa
Prince Ofori <princeofori723@gmail.com>
2022-09-09 18:42:16 -0700

    sollution task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabe
+ * Description: print the letters of the aphabet in lower then in uppercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b;
+	a = 97;
+	b = 65;
+	while (a < 123)
+	{
+		putchar(i);
+		a++;
+	}
+	while (b < 91)
+	{
+		putchar(j);
+		b++;
+	}
+	putchar(10);
+return (0);
+}

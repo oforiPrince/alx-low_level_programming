0c708b53c76bc170897acf524b0c06cb93172ea8
Prince Ofori <princeofori723@gmail.com>
2022-09-13 07:10:17 -0700

    fix error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  main - prints all the alphabets
+ *  print_alphabet - prints all the alphabets
  *  Description: using the main function
  *  this program prints all alphabets
  *  Return: 0
@@ -12,7 +12,7 @@ void print_alphabet(void)
 	char start = 'a';
 	char end = 'z';
 
-	while(start < end)
+	while (start < end)
 	{
 		_putchar(start);
 		start++;

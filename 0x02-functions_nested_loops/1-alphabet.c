c86073df60e2b26618742f7570d1964e8ec07039
Prince Ofori <princeofori723@gmail.com>
2022-09-13 07:02:53 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  main - prints all the alphabets
+ *  Description: using the main function
+ *  this program prints all alphabets
+ *  Return: 0
+*/
+
+void print_alphabet(void)
+{
+	char start = 'a';
+	char end = 'z';
+
+	while(start < end)
+	{
+		_putchar(start);
+		start++;
+	}
+	_putchar('\n');
+}

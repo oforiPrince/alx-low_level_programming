dd0206507a9dd66d649b9a99e1c789da67ba5406
Prince Ofori <princeofori723@gmail.com>
2022-09-13 08:02:32 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * is_lower - print 1 if alphabet is lower case or 0 if alphabe is not
+ * Description: using -s_lower function
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	return (c > 97 && c < 122);
+}

d546f08897c1927d0854d0064b774d14d07d922b
Prince Ofori <princeofori723@gmail.com>
2022-09-13 05:29:19 -0700

    task 1l

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>;
+#include "main.h";
+/**
+ * main: prints _putchar
+ * Description: a program that prints _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char myWord[8] = "_putchar";
+	
+	for (int i = 0; i < strlen(myWord); i++)
+	{
+		_putchar(myWord[i]);
+	}
+	
+	_putchar('\n');
+	return (0);
+}

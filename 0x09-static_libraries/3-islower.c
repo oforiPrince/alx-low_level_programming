3f65ff5c577d21a9fb3144b5ca02a90c395ea300
oforiPrince <pofori034@st.ug.edu.gh>
2022-10-03 12:58:52 +0000

    upload

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - print 1 if alphabet is lower case or 0 if alphabe is not
+ * @c : character to check the case
+ * Description: using -s_lower function
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	return (c > 97 && c < 122);
+}
